3d99e02227f15b2c8c6621d7a290f8d592d641ce
Trevor Rounce <trevor@switchbackcreative.ca>
2025-10-06 14:41:35 -0600

    SEO setup

---
+++ src/data/siteData.js
@@ -0,0 +1,8 @@
+import defaultSrc from "../../public/meta-default-image.jpg";
+
+export const baseData = {
+  title: "Goldwing Accounting",
+  description: "Goldwing Accountingn",
+  siteImg: `https://goldwingaccounting.netlify.app/${defaultSrc.src}`,
+  url: "https://goldwingaccounting.netlify.app/",
+};
